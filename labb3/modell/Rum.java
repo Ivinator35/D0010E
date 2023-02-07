50a5f0bd4555f4f8e7c537fb91688fee6c18083a
Ivar Wirgén <ivawir-2@student.ltu.se>
2023-02-07 16:44:49 +0100

    fix

--- labb3/modell/Rum.java
+++ labb3/modell/Rum.java
@@ -6,10 +6,51 @@ public class Rum {
 
 	// TODO: Lägg till tillståndsvariabler.
 
+	protected int bredd;
+	protected int höjd;
+	protected int övX;
+	protected int övY;
+	protected Color golvfärg;
+
 	public Rum(Color golvfärg, int bredd, int höjd, int övX, int övY) {
+		this.bredd = bredd;
+		this.höjd = höjd;
+		this.övX = övX;
+		this.övY = övY;
+
 		// TODO: Kopiera parametrarna in i tillståndsvariablerna. (övX,övY) är
 		// koordinaten för rummets övre vänstra hörn och lagras lämpligen som en
 		// instans av klassen Punkt i paketet verktyg.
+	}
+
+	public Color getGolvfärg(){
+		return golvfärg;
+	}
+
+	public int getBredd(){
+		return bredd;
+	}
+
+	public int getHöjd(){
+		return höjd;
+	}
+
+	public int getÖvX(){
+		return övX;
+	}
+
+	public int getÖvY(){
+		return övY;
+	}
+
+
+	public boolean finnsUtgångÅt(Väderstreck väderstreck){
+		if(finnsengång){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	// TODO: Skriv "getters", metoder som returnerar tillståndsvariablernas
@@ -22,6 +63,8 @@ public class Rum {
 	// som ska kontrollera om det från ett rum finns en utgång åt visst
 	// väderstreck.
 
+
+	
 	// TODO: Skriv instansmetoden
 	//
 	// Gång gångenÅt(Väderstreck väderstreck) som
@@ -30,6 +73,13 @@ public class Rum {
 	// sådan gång saknas ska ett undantag kastas med lämpligt felmeddelande.
 
 	// TODO: Skrivklar metoden nedan som kopplar ihop två rum med en gång.
+
+	public Gång gångenÅt(Väderstreck väderstreck){
+		if(gång.getRIIT() == ){
+			
+		}
+	}
+
 
 	public static void kopplaIhop(Rum från, Väderstreck riktningUtUrFrån,
 			Rum till, Väderstreck riktningInITill) {
--- labb3/modell/Väderstreck.java
+++ labb3/modell/Väderstreck.java
@@ -2,6 +2,31 @@ package labb3.modell;
 
 public enum Väderstreck {
 	NORR, ÖSTER, SÖDER, VÄSTER;
+
+	protected int tempIndex;
+
+	public Väderstreck(int tempIndex){
+		this.tempIndex = tempIndex;
+	}
+
+	public int index(){
+
+		if(NORR){
+			tempIndex = 0;
+		}
+		else if(ÖSTER){
+			tempIndex = 1;
+		}
+		else if(SÖDER){
+			tempIndex = 2;
+		}
+		else if(VÄSTER){
+			tempIndex = 3;
+		}
+
+		return tempIndex;
+
+	}
 	// TODO: Lägg till en konstruktor som tar ett heltal och sparar det i 
 	// en dold heltalsvariabel (en instansvariabel). Skriv en instansmetod
 	// index() som returnerar heltalsvariabeln. Ändra de fyra väderstrecken 
