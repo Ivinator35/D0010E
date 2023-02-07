5c630835f094db2367711401d8e44ec816da3f2b
Ivar Wirgén <ivawir-2@student.ltu.se>
2023-02-07 16:45:06 +0100

    Fix

--- labb3/modell/Nivå.java
+++ labb3/modell/Nivå.java
@@ -3,26 +3,62 @@ package labb3.modell;
 import java.util.ArrayList;
 
 // TODO: Gör så att klassen Nivå ärver Observable i paketet java.util. 
-public class Nivå {
+public class Nivå extends Observable {
+
+	ArrayList<Rum> rList = new ArrayList()<>;
+
+	protected Rum startrum;
+
+	protected int count = 0;
 
 	// TODO: Lägg till tillståndsvariabler för att hålla reda på nivåns rum och
 	// i vilket rum som användaren "är".
 
 	public Nivå(Rum startrum, ArrayList<Rum> rum) {
+
 		// TODO: Kopiera in startrum och rum in i tillståndsvariablerna.
 
+		this.startrum = startrum;
+		this.rList = rum;
+		
 		// TODO: Kontrollera att startrum finns med i rum. Om inte, kasta
 		// undantag med lämpligt felmeddelande.
+		
+		for(int i = 0; i < rList.length; i++){
+			if(rum[i] == startrum){
+				count++;
+			}
+		}
+		if(count <= 0){
+			throw new Exception("Start rummet finns inte i nivån");
+		}
 
 		// TODO: Kontrollera att inga rum överlappar varandra. Om det ändå är
 		// fallet, kasta undantag med lämpligt felmeddelande.
+
+		for(int i = 1; i < rList.length; i++){
+			if(rum[i].getÖvX == rum[i-1].getÖvX && rum[i].getÖvY == rum[i-1].getÖvY ){
+				throw new Exception("Det finns rum i nivån som överlappar");
+			}
+		}
+
 	}
 
 	// TODO: Skriv en instansmetod som returnerar alla rummen. Denna behöver
 	// Målarduk för att veta vilka rum som finns på nivån och som ska ritas ut.
 
+	
+
 	// TODO Skriv en instansmetod som returnerar en referens till det rum som
 	// användaren "är i".
+
+	public Rum getPlayerPos(){
+		for(int i = 0; i < rList.length; i++){
+			if(playerPos.x == (rum[i].getÖvX / 2) && playerPos.y == (rum[i].getÖvY / 2)){
+				return rum[i];
+			}
+		}
+	}
 
 	// TODO: Skriv klar instansmetoden hoppaÅt nedan så att den ändrar det rum
 	// som användaren "är i" om det är möjligt genom att följa en gång från
